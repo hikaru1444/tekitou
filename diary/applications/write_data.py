9626db795aabdabedc903fd058902fa666bc68dc
hikaru1444 <82006837+hikaru1444@users.noreply.github.com>
2021-11-14 16:46:20 +0900

    Delete diary directory

--- diary/applications/write_data.py
+++
@@ -1,8 +0,0 @@
-import os
-import csv
-
-def write_csv(data):
-    datas = {data}
-    with open(os.getcwd()+'diary/applications/'+'data.csv','a') as f:
-        write = csv.writer(f, lineterminator='\n')
-        writer.writerow(datas)
